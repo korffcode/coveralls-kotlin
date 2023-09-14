4a5afa8db3585813af78e78defab94eb91b03332
Bryan Korff <bryan.korff@warnermedia.com>
2023-09-14 09:04:54 -0400

    Coveralls test:  commented out some tests to test pr blocking.

--- src/test/kotlin/SeasonsAverageTest.kt
+++ src/test/kotlin/SeasonsAverageTest.kt
@@ -54,7 +54,6 @@ internal class SeasonsAverageTest {
         assertEquals(expected, testSeasonsAverage.getAwayTeamName(testString))
     }
 
-	/*
     @Test
     fun testGetHomeTeamScore() {
         val expected = 35
@@ -82,7 +81,6 @@ internal class SeasonsAverageTest {
 
         assertEquals(expected, testSeasonsAverage.getStadium(testString))
     }
-	*/
 
     @Test
     fun testMain() {
